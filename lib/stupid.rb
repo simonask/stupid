3af8114b383a4a8def359453979f0361962070dc
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-09-20 12:27:51 +0200

    added current dir to load path

--- lib/stupid.rb
+++ lib/stupid.rb
@@ -5,6 +5,8 @@ STUPID_ROOT = Pathname.new(File.dirname(__FILE__) + "/..").realpath.to_s
 
 STUPID_LOAD_DIRS = ['lib', 'config', 'app']
 
+$LOAD_PATH << File.dirname(__FILE__)
+
 STUPID_LOAD_DIRS.each do |dir|
 	Dir.glob("#{STUPID_ROOT}/#{dir}/**/*.rb").each do |file|
 		require file
